c3653a568a570124023407c2a225f7d41e5c23bb
Bojtronic <bojtronic@gmail.com>
2024-12-10 23:58:07 -0600

    Codigo del cliente

---
+++ ProyectoPkdx/Api_Pdx_Db_V2/Api_Pdx_Db_V2/Data/DbConexionContext.cs
@@ -0,0 +1,31 @@
+﻿using Api_Pdx_Db_V2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api_Pdx_Db_V2.Data
+{
+    public class DbConexionContext : DbContext
+    {
+        public DbConexionContext(DbContextOptions<DbConexionContext> options) : base(options) { }
+
+        public DbSet<RolModel> rol { get; set; }
+        public DbSet<UsuarioModel> usuario { get; set; }
+        public DbSet<UsuarioRolModel> usuario_rol { get; set; }
+        public DbSet<EstadoModel> estado { get; set; }
+        public DbSet<UsuarioPkmModel> usuario_pkm { get; set; }
+        public DbSet<UsuarioPktModel> usuario_pocket {  get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<RolModel>().ToTable("rol");
+
+            modelBuilder.Entity<UsuarioModel>().ToTable("usuario");
+
+            modelBuilder.Entity<UsuarioRolModel>().ToTable("usuario_rol");
+
+            modelBuilder.Entity<EstadoModel>().ToTable("estado");
+
+            modelBuilder.Entity<UsuarioPkmModel>().ToTable("usuario_pkm");
+
+            modelBuilder.Entity<UsuarioPktModel>().ToTable("usuario_pocket");
+        }
+    }
+}
---
+++ ProyectoPkdx/Api_Pdx_Db_V2/Api_Pdx_Db_V2/Models/UsuarioPktModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Api_Pdx_Db_V2.Models
+{
+    public class UsuarioPktModel
+    {
+        public int Id { get; set; }
+        public int IdUsuario { get; set; }
+        public int pkm_Id1 { get; set; }
+        public int pkm_Id2{ get; set; }
+        public int pkm_Id3 { get; set; }
+    }
+}
